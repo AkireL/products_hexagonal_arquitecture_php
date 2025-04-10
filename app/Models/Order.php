55515a55e24ac0e18a337a9e84f501941ba3982b
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-04-09 19:07:37 -0600

    added migrations and models

---
+++ app/Models/Order.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'order_products')
+                    ->withPivot(
+                        'quantity',
+                        'product_unit_price',
+                        'product_description',
+                        'total_price',
+                    )
+                    ->withTimestamps();
+    }
+}
