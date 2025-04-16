9ceafd58628afc6bce9cec24cfb0f11c44cc1d76
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-04-16 15:53:04 -0600

    added a new endpoint

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('user', UserController::class)->except(['show', 'index']);
 Route::apiResource('product', ProductController::class);
-Route::apiResource('user/order', OrderUserController::class);
+Route::apiResource('user/order', OrderUserController::class)->only(['show', 'store']);
+Route::post('user/order/{order}/product', [OrderUserController::class, 'clear']);
