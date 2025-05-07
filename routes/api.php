9ad476ee8bd2560b70542378907191b125205c07
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-05-06 18:56:10 -0600

    added name to endpoint

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,4 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('user', UserController::class)->except(['show', 'index']);
 Route::apiResource('product', ProductController::class);
 Route::apiResource('user/order', OrderController::class)->only(['show', 'store']);
-Route::post('user/order/{order}/product', [OrderController::class, 'clear']);
+Route::post('user/order/{order}/product', [OrderController::class, 'clear'])->name('order.products.clear');
